4996a5142f3aef4a4a395b4bf85b492d156e5bc0
kikugaw <kk.019.nick@gmail.com>
2024-07-13 15:12:41 +0900

    ページヘッダー作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Categories } from './components';
 import './index.css';
-import { PageNavigation } from './layouts/PageNavigation';
+import { PageHeader, PageNavigation } from './layouts';
 
 export default function App() {
   return (
     <div>
       <div className="sticky top-0 z-10 bg-white">
+        <PageHeader />
         <Categories />
       </div>
       <div className="min-h-screen"></div>
