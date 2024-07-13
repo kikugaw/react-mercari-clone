db1fc06bc4dcfa02e88856ccb0fe954055ce4525
kikugaw <kk.019.nick@gmail.com>
2024-07-13 10:47:48 +0900

    カテゴリ表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Categories } from './components';
+import './index.css';
 import { PageNavigation } from './layouts/PageNavigation';
+
 export default function App() {
   return (
     <div>
+      <div className="sticky top-0 z-10 bg-white">
+        <Categories />
+      </div>
+      <div className="min-h-screen"></div>
       <PageNavigation />
     </div>
   );
